283f1cbeae38251718652aaea474daccc4d59189
BritishFan <fabmata04@gmail.com>
2015-07-24 11:59:34 +0530

    Initial commit

---
+++ app/src/main/java/com/mcacarrers/messanger/CreateMessageActivity.java
@@ -0,0 +1,30 @@
+package com.mcacarrers.messanger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //intent.putExtra("message",messageText);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+
+}
